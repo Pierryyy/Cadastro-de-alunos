a28b3cb9e71b8da785a0877b5c0e0095f66cf3de
Pierry Derungs <derungspierry15@gmail.com>
2021-08-23 21:56:44 -0300

    aaa

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,6 +7,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _registerController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +27,49 @@ class _HomeState extends State<Home> {
                 border: OutlineInputBorder(),
               ),
               keyboardType: TextInputType.number,
+              controller: _registerController,
             ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nome do Aluno',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.name,
+              controller: _nameController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email do Aluno',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+              controller: _emailController,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: register,
+              child: Text('Cadastrar aluno'),
+            )
           ],
         ),
       ),
     );
+  }
+
+  void register() async {
+    String name = _nameController.text;
+    String email = _emailController.text;
+
+    if (!EmailValidator.validate(email)) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Mensagem do Sistema'),
+          content: Text('Email inválido!!!'),
+        ),
+      );
+    } else {}
   }
 }
